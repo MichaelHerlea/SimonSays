4999a9fe70ee7ae5da6b1dc4ed2d2f24764ee5b9
MichaelHerlea <mihaijherlea+github@gmail.com>
2025-06-29 19:32:28 +0200

    Functional GUI

--- colorMemory.py
+++ colorMemory.py
@@ -1,13 +1,14 @@
 import random
 import pygame
+import time
 
 pygame.init()
 window = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Simon says")
 clock = pygame.time.Clock()
 
-sequenceList = []
-gameRun = True
+gameSequence = []
+userInputtedSequence = []
 
 def draw():
     pygame.draw.rect(window, "green", (0, 0, 400, 400))
@@ -15,30 +16,64 @@ def draw():
     pygame.draw.rect(window, "yellow", (0, 400, 400, 400))
     pygame.draw.rect(window, "blue", (400, 400, 400, 400))
 
-print("When inputting the sequence, seperate each value by a space")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            mousePos = pygame.mouse.get_pos()
+            if mousePos[0] <= 400 and mousePos[1] <= 400:
+                userInputtedSequence.append(0)
+            if mousePos[0] >= 400 and mousePos[1] <= 400:
+                userInputtedSequence.append(1)
+            if mousePos[0] <= 400 and mousePos[1] >= 400:
+                userInputtedSequence.append(2)
+            if mousePos[0] >= 400 and mousePos[1] >= 400:
+                userInputtedSequence.append(3)
 
-while gameRun == True:
-    sequenceList.append(random.randint(0, 3))
-    print(sequenceList)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    if len(userInputtedSequence) == len(gameSequence):
+        for i in range(len(gameSequence)):
+            if userInputtedSequence[i] != gameSequence[i]:
                 pygame.quit()
-                gameRun = False
                 exit()
-        
-        draw()
-        pygame.display.update()
-        clock.tick(60)
+        userInputtedSequence = []
+        gameSequence.append(random.randint(0, 3))
+        print(gameSequence)
+        for i in range(len(gameSequence)):
+            if gameSequence[i] == 0:
+                draw()
+                pygame.draw.rect(window, "white", (0, 0, 400, 400))
+                pygame.display.update()
+                time.sleep(1)
+                draw()
+                pygame.display.update()
+                time.sleep(0.5)
+            if gameSequence[i] == 1:
+                draw()
+                pygame.draw.rect(window, "white", (400, 0, 400, 400))
+                pygame.display.update()
+                time.sleep(1)
+                draw()
+                pygame.display.update()
+                time.sleep(0.5)
+            if gameSequence[i] == 2:
+                draw()
+                pygame.draw.rect(window, "white", (0, 400, 400, 400))
+                pygame.display.update()
+                time.sleep(1)
+                draw()
+                pygame.display.update()
+                time.sleep(0.5)
+            if gameSequence[i] == 3:
+                draw()
+                pygame.draw.rect(window, "white", (400, 400, 400, 400))
+                pygame.display.update()
+                time.sleep(1)
+                draw()
+                pygame.display.update()
+                time.sleep(0.5)
 
-    responseList = input().split()
-    for i in range(len(responseList)):
-        responseList[i] = int(responseList[i])
-
-    if len(responseList) == len(sequenceList):
-        for i in range(len(sequenceList)):
-            if responseList[i] != sequenceList [i]:
-                gameRun = False
-    else:
-        gameRun = False
+    draw()
+    pygame.display.update()
+    clock.tick(60)
